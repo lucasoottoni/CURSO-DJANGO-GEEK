3a0dcfe7ac22c18f7e4a0f16cb2effdad2736891
Lucas Oliveira Ottoni <lucas.loo@pf.gov.br>
2023-11-21 18:59:07 -0300

    second-commit

--- escola/settings.py
+++ escola/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    #extralibs
+    'django_filters',
+    'rest_framework',
+    
+    #myapps
     'cursos'
 ]
 
@@ -126,3 +131,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+#DRF
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    )
+}
